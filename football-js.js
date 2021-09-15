c590277ab24859cbe19b0e3812c52ec467efb046
Ziv <zivshaul3@gmail.com>
2021-09-15 12:13:38 +0300

    Fixed football
    I felt bad that I did it so wrong yesterday so I did it again but this time better

--- football-js.js
+++ football-js.js
@@ -1,31 +1,20 @@
 const aspectRatio = "01:01"
-const size = 10
+const fieldSize = 500
+const statiumSize = 15
 const ballSize = 30
 
 
 
-let testTable = []
 const field = document.getElementById("field")
 
-const createRow = (rowLength, row) => {
-    for (let r=0 ; r<rowLength; r++){
-            let tile = document.createElement("td")
-            tile.textContent = ""
-            row.appendChild(tile)
-        }
-        return row
-}
+
 
 function createField(size, aspectRatio){
-    const field = document.getElementById("field")
-    const rowsNum = parseInt(aspectRatio.slice(0,Math.floor(aspectRatio.length/2)))*size
-    const rowLength = parseInt(aspectRatio.slice(Math.ceil(aspectRatio.length/2)))*size
-    for (let h = 0; h < rowsNum; h++) {
-        let row = document.createElement("tr")
-        createRow(rowLength, row)
-        
-        field.appendChild(row)
-    }
+    const h = parseInt(aspectRatio.slice(0,Math.floor(aspectRatio.length/2)))*size
+    const w = parseInt(aspectRatio.slice(Math.ceil(aspectRatio.length/2)))*size
+    field.style.width = `${w}px`
+    field.style.height = `${h}px`
+    field.style.borderWidth = `${statiumSize}px`
     return field
 }
 
@@ -35,29 +24,33 @@ function createBall(ballSize){
     ball.textContent = "⚽"
     ball.style.textAlign = "center"
     ball.style.fontSize = `${ballSize}px`
-    
-    // startingPoint.textContent = "⚽"
+    ball.style.position = "absolute"
     return ball
 }
 function createBallOnField(ballSize){
-    const startingPoint = document.querySelector("td")
+    // const startingPoint = document.querySelector("#field")
     const ball = createBall(ballSize)
-    startingPoint.appendChild(ball)
+    field.appendChild(ball)
     
     // startingPoint.textContent = "⚽"
 }
 
-createField(size, aspectRatio)
+createField(fieldSize, aspectRatio)
 createBallOnField(ballSize)
 
-function moveBall(target){
-    const currentPosition = document.querySelector("#ball")
-    currentPosition.parentElement.removeChild(currentPosition)
-    const newPosition = target
-    const ball = createBall(ballSize)
-    newPosition.appendChild(ball)
+function moveBall({clientX, clientY}){
+    const ball = document.getElementById("ball")
+    let fieldCoords = field.getBoundingClientRect();
+    let BallX = clientX - ball.clientWidth/2 
+    let BallY = clientY - ball.clientHeight/2
+    if (BallX > fieldCoords.width-statiumSize-(ball.clientWidth/2)-fieldCoords.x) BallX = fieldCoords.width-statiumSize-(ball.clientWidth/2)-fieldCoords.x
+    if (BallX < fieldCoords.left+ball.clientWidth/2) BallX = fieldCoords.left/2+statiumSize
+    if (BallY > fieldCoords.height-ball.clientHeight-statiumSize) BallY = fieldCoords.height-ball.clientHeight-fieldCoords.y
+    if (BallY < fieldCoords.top+ball.clientHeight/2) BallY = fieldCoords.top/2+statiumSize
+    ball.style.left = BallX + 'px';
+    ball.style.top = BallY + 'px';
+    console.log(fieldCoords)
+
 }
 
-// const field = document.getElementById("field")
-// field.addEventListener("click", ()=>moveBall({target}))
-field.addEventListener("click", ({target})=>{moveBall(target)})
+field.addEventListener("click", moveBall)
